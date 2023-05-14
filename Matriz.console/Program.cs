19d77553bab6105eabf9057826c12cd8379c3caa
Jorge Cadavid <cadavidjorge10@gmail.com>
2023-05-13 22:13:00 -0500

    cambios

--- Matriz.console/Program.cs
+++ Matriz.console/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("**********************");
+Console.WriteLine("        MATRIZ        ");
+Console.WriteLine("**********************");
+
+
+Console.Write("Enter the value of m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the value of n: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the value of p: ");
+int p = int.Parse(Console.ReadLine());
+
+int[,] A = new int[m, n];
+int[,] B = new int[n, p];
+int[,] C = new int[m, p];
